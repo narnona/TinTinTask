1f545c3c69d81f21e381e23e5f06884b5b487737
kaka <tolele666@gmail.com>
2024-09-30 16:26:09 +0800

    Create Ballot_03.sol

---
+++ Ballot_03.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Ballot {
+    struct Voter {
+        uint256 weight;
+        bool voted;
+        address delegate;
+        uint256 vote;
+    }
+
+    struct Proposal {
+        bytes32 name;
+        uint256 voteCount;
+    }
+
+    address public chairperson;
+    mapping(address => Voter) public voters;
+    Proposal[] public proposals;
+
+    // 投票开始时间与结束时间
+    uint256 startTime;
+    uint256 endTime;
+
+    // voteTime：投票时间间隔
+    constructor(bytes32[] memory proposalNames, uint256 voteTime) {
+        chairperson = msg.sender;
+        voters[chairperson].weight = 1;
+
+        // 初始化时间变量
+        startTime = block.timestamp;
+        endTime = block.timestamp + voteTime;
+
+        for (uint256 i = 0; i < proposalNames.length; i++) {
+            proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
+        }
+    }
+
+    function giveRightToVote(address voter) external {
+        require(msg.sender == chairperson, "Only chairperson can give right to vote.");
+        require(!voters[voter].voted, "The voter already voted.");
+        require(voters[voter].weight == 0, "The voter already has voting rights.");
+
+        voters[voter].weight = 1;
+    }
+
+    // 设置权重
+    function setVoterWeight(address voter, uint weight) external {
+        require(msg.sender == chairperson, "Only chairperson can give right to vote.");
+        voters[voter].weight = weight;
+    }
+
+    function delegate(address to) external {
+        Voter storage sender = voters[msg.sender];
+        require(sender.weight != 0, "You have no right to vote.");
+        require(!sender.voted, "You already voted.");
+        require(to != msg.sender, "Self-delegation is disallowed.");
+
+        while (voters[to].delegate != address(0)) {
+            to = voters[to].delegate;
+
+            require(to != msg.sender, "Found loop in delegation.");
+        }
+
+        Voter storage delegate_ = voters[to];
+        require(delegate_.weight >= 1, "Selected delegate does not have voting rights.");
+        sender.voted = true;
+        sender.delegate = to;
+        if (delegate_.voted) {
+            proposals[delegate_.vote].voteCount += sender.weight;
+        } else {
+            delegate_.weight += sender.weight;
+        }
+    }
+
+    function vote(uint256 proposal) external {
+        // 检查投票是否已结束
+        require(block.timestamp < endTime, "Vote period has ended.");
+        Voter storage sender = voters[msg.sender];
+        require(sender.weight != 0, "Has no right to vote.");
+        require(!sender.voted, "Already voted.");
+        sender.voted = true;
+        sender.vote = proposal;
+        proposals[proposal].voteCount += sender.weight;
+    }
+
+    function winningProposal() public view returns (uint256 winningProposal_) {
+        uint256 winningVoteCount = 0;
+        for (uint256 p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
+    }
+
+    function winnerName() external view returns (bytes32 winnerName_) {
+        winnerName_ = proposals[winningProposal()].name;
+    }
+}
